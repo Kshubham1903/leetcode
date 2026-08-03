4d65fef5e6b8ec4c2b3a6d9f16fbf42984754692
Shubham Bhagvant Kadam <shubhamkadamsk193@gmail.com>
2026-08-03 21:55:28 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (96.58%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,28 +1,28 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        int n = haystack.length();
-        int m = needle.length();
-        if(haystack.contains(needle)){
-            int j = 0;
-                for(int i = 0; i < haystack.length(); i++ ){
-                    if(haystack.charAt(i) == needle.charAt(j)){
-                        int k = i;
+        // int n = haystack.length();
+        // int m = needle.length();
+        // if(haystack.contains(needle)){
+        //     int j = 0;
+        //         for(int i = 0; i < haystack.length(); i++ ){
+        //             if(haystack.charAt(i) == needle.charAt(j)){
+        //                 int k = i;
                 
-                        while(k < n && j < m ){
-                            if(haystack.charAt(k) == needle.charAt(j)){
-                                k++;j++;
-                                if(j == m) return ( k - j );
-                            }
-                            else {
-                                j = 0;
-                                break;
-                            }
+        //                 while(k < n && j < m ){
+        //                     if(haystack.charAt(k) == needle.charAt(j)){
+        //                         k++;j++;
+        //                         if(j == m) return ( k - j );
+        //                     }
+        //                     else {
+        //                         j = 0;
+        //                         break;
+        //                     }
 
-                        }
+        //                 }
 
-                    }
-                }
-        }
-        return -1;
+        //             }
+        //         }
+        // }
+        return haystack.indexOf(needle);
     }
 }
