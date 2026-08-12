80712ec9aec82bd287d7f313a7601292d53c4205
Shubham Bhagvant Kadam <shubhamkadamsk193@gmail.com>
2026-08-12 22:12:24 +0530

    Time: 29 ms (93.48%) | Memory: 58.9 MB (86.34%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,34 +1,41 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-
-        Set<List<Integer>> result = new HashSet<>();
-
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
         int n = nums.length;
+        for(int i = 0;i < nums.length; i++){
+            if( i > 0 && nums[i] == nums[i-1] ) continue;
 
-        for (int i = 0; i < n - 2; i++) {
+            int left = i + 1;
+            int right = n - 1;
+            while(left < right){
+                int sum = nums[ i ] + nums[ left ] + nums[ right ];
+                if(sum < 0){
+                    left++;
 
-            Set<Integer> set = new HashSet<>();
-
-            for (int j = i + 1; j < n; j++) {
-
-                int required = -(nums[i] + nums[j]);
-
-                if (set.contains(required)) {
-
-                    List<Integer> triplet = Arrays.asList(
-                        nums[i],
-                        nums[j],
-                        required
-                    );
-
-                    Collections.sort(triplet);
-                    result.add(triplet);
                 }
+                else if(sum > 0){
+                    right--;
+                }
+                else{
+                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
 
-                set.add(nums[j]);
+                    while(left < right && nums[left] == nums[left + 1 ] ){
+                            left++;
+                    }
+                    while(left < right && nums[right] == nums[right - 1 ]){
+                        right--;
+                }
+                left++;
+                right--;
+
             }
+
         }
 
-        return new ArrayList<>(result);
+        
+        
     }
+    return ans;
+}
 }
