1efc2121a744c0e495ba3ffb48b5c8a3315a1a77
Shubham Bhagvant Kadam <shubhamkadamsk193@gmail.com>
2026-09-23 22:19:21 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (43.39%) - LeetSync

---
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.java
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(>val) { val = val; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        // Copy the value of the next node to the current node
+        node.val = node.next.val;
+        // Bypass the next node
+        node.next = node.next.next;
+    }
+}
